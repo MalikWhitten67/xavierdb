ea80917384892f860cd57a7433b37e9ac686cfc8
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2024-06-20 10:33:00 -0600

    build directly to a builds folder

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 echo "Build started at $(date)"
-bun build --compile --sourcemap=inline --minify --target=bun-windows-x64 entry.ts --outfile ./windows/x64/xavier
-bun build --compile --sourcemap=inline --minify --target=bun-linux-arm64 entry.ts --outfile ./linux/arm64/xavier
-bun build --compile --sourcemap=inline --minify --target=bun-linux-x64 entry.ts --outfile ./linux/x64/xavier
-bun build --compile --sourcemap=inline --minify --target=bun-darwin-arm64 entry.ts --outfile ./darwin/arm64/xavier 
+bun build --compile --sourcemap=inline --minify --target=bun-windows-x64 entry.ts --outfile ./builds/windows/x64/xavier
+bun build --compile --sourcemap=inline --minify --target=bun-linux-arm64 entry.ts --outfile ./builds/linux/arm64/xavier
+bun build --compile --sourcemap=inline --minify --target=bun-linux-x64 entry.ts --outfile ./builds/linux/x64/xavier
+bun build --compile --sourcemap=inline --minify --target=bun-darwin-arm64 entry.ts --outfile ./builds/darwin/arm64/xavier 
 echo "Build completed at $(date)"
